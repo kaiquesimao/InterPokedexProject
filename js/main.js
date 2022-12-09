ca662f06bcf7ce745a9e52d1c7cc7dfe31764680
kaiquesimao <kaique.gabriel.me@gmail.com>
2022-12-09 12:54:51 -0300

    refatoração para organização do código e trocar for por map

--- js/main.js
+++ js/main.js
@@ -1,34 +1,25 @@
-buscarPokemons();
-
-function buscarPokemons() {
-  const offset = 0;
-  const limit = 9;
-  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-
-  fetch(url)
-    .then((response) => response.json())
-    .then((JsonBody) => JsonBody.results)
-    .then((pokemons) => {
-      for (let i = 0; i < pokemons.length; i++) {
-        let pokemon = pokemons[i];
-        convertPokemonToHtml(pokemon);
-      }
-    })
-    .catch((error) => console.log(error));
-}
+let pokemonList = document.getElementById("pokemonList");
+pokeApi
+  .getPokemons()
+  .then((pokemons = []) => {
+    const newList = pokemons.map((pokemon) => convertPokemonToHtml(pokemon));
+    const newHtml = newList.join("");
+    pokemonList.innerHTML += newHtml;
+    return pokemonList;
+  })
+  .catch((error) => console.log(error));
 
 function convertPokemonToHtml(pokemon) {
-  let pokemonList = document.getElementById("pokemonList");
-  pokemonList.innerHTML += `<li class="pokemon">
-            <span class="numeroPokemon">#001</span>
-            <span class="nome">${pokemon.name}</span>
+  return `<li class="pokemon">
+    <span class="numeroPokemon">#001</span>
+        <span class="nome">${pokemon.name}</span>
             <div class="detalhes">
                 <ol class="tipos">
                     <li class="tipo">Grass</li>
                     <li class="tipo">Poison</li>
                 </ol>
                 <img alt="${pokemon.name}"
-                     src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg">
+                    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg">
             </div>
         </li>`;
 }
