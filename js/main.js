08e85a0869675ec2889fa96750daa249d7360839
kaiquesimao <Kaique.gabriel.me@gmail.com>
2022-12-19 15:14:22 -0300

    Implementado paginação de 10 em 10

--- js/main.js
+++ js/main.js
@@ -1,22 +1,31 @@
-let pokemonList = document.getElementById("pokemonList");
-pokeApi.getPokemons().then((pokemons = []) => {
-  pokemonList.innerHTML = pokemons.map(convertPokemonToHtml).join("");
-});
+const pokemonList = document.getElementById("pokemonList");
+const loadMorePokeButton = document.getElementById("loadMorePokemons");
+let offset = 0;
+const limit = 10;
 
-function convertPokemonToHtml(pokemon) {
-  return `
-    <li class="pokemon ${pokemon.type}">
-    
-      <span class="numeroPokemon">#${pokemon.number}</span>
-      <span class="nome">${pokemon.name}</span>
+loadPokemons(offset, limit);
+
+function loadPokemons (offset, limit){  
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    const newHtml = pokemons.map((pokemon) => `
+      <li class="pokemon ${pokemon.type}">
       
-      <div class="detalhes">
-        <ol class="tipos">
-          ${pokemon.types
-            .map((type) => `<li class="tipo ${type}">${type}</li>`)
-            .join("")}
-        </ol>
-          <img alt="${pokemon.name}" src="${pokemon.photo}">
-      </div>
-    </li>`;
+        <span class="numeroPokemon">#${pokemon.number}</span>
+        <span class="nome">${pokemon.name}</span>
+        
+        <div class="detalhes">
+          <ol class="tipos">
+            ${pokemon.types
+              .map((type) => `<li class="tipo ${type}">${type}</li>`)
+              .join("")}
+          </ol>
+            <img alt="${pokemon.name}" src="${pokemon.photo}">
+        </div>
+      </li>`).join("");
+    pokemonList.innerHTML += newHtml; 
+  });
 }
+
+loadMorePokeButton.addEventListener("click", () => {
+  offset += limit
+  loadPokemons(offset, limit)});
